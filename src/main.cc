25ee1510382a167caf1c4fadb3c26df34636ab87
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2022-06-29 00:03:28 +0200

    main: destroy main fiber and whole cord on return
    main() used to skip most of modules destruction in
    tarantool_free(). That got ASAN complaining on clang-13 about a
    leak of a fiber on_stop trigger which was allocated in Lua.
    
    The patch makes fiber_free() called for the main cord. It destroys
    and frees all the fibers together with their on_stop triggers.
    
    Closes #7259
    
    NO_CHANGELOG=Not a visible change
    NO_DOC=Not a visible change
    NO_TEST=Not a visible change

--- src/main.cc
+++ src/main.cc
@@ -588,7 +588,6 @@ tarantool_free(void)
 	tarantool_lua_free();
 	session_free();
 	user_cache_free();
-	fiber_free();
 	memory_free();
 	random_free();
 #endif
@@ -597,6 +596,7 @@ tarantool_free(void)
 	coll_free();
 	systemd_free();
 	say_logger_free();
+	fiber_free();
 }
 
 static void
