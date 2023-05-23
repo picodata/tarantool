3ad3b534b10a0090e55a67df097927c4830219ef
Kaitmazian Maksim <m.kaitmazian@picodata.io>
2023-05-23 17:48:26 +0300

    feat: implement md5 authentication
    It prevents password sniffing and avoids storing passwords on the
    server in plain text but provides no protection if an attacker
    manages to steal the password hash from the server.
    
    Usage example:
    ```lua
    -- Enable the md5 authentication method for all new users.
    box.cfg({auth_type = 'md5'})
    
    -- Reset existing user passwords to use the md5 authentication method.
    box.schema.user.passwd('alice', 'topsecret')
    
    -- Authenticate using the md5 authentication method via net.box.
    conn = require('net.box').connect(uri, {
        user = 'alice',
        password = 'topsecret',
        -- Specifying the authentication method isn't strictly necessary:
        -- by default the client will use the method set in the remote
    	-- server config (box.cfg.auth_type)
        auth_type = 'md5',
    })
    ```
    
    part of picodata/picodata/sbroad!377
    
    @TarantoolBot document
    Title: md5 authentication method
    
    See the commit message.

---
+++ src/lib/core/crypt.h
@@ -0,0 +1,44 @@
+/**
+ * PostgreSQL Database Management System
+ * (formerly known as Postgres, then as Postgres95)
+ *
+ * Portions Copyright (c) 1996-2023, PostgreSQL Global Development Group
+ *
+ * Portions Copyright (c) 1994, The Regents of the University of California
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without a written agreement
+ * is hereby granted, provided that the above copyright notice and this
+ * paragraph and the following two paragraphs appear in all copies.
+ *
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATIONS TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+#pragma once
+
+#include <stddef.h>
+#include "md5.h"
+
+/**
+ * Computes MD5 checksum of a password followed by salt.
+ *
+ * @param password password to be encrypted (need not be null-terminated)
+ * @param password_len length of the password
+ * @param salt salt used for encryption (need not be null-terminated)
+ * @param salt_len len of salt string. May differ from MD5_SALT_LEN if
+ * user name is used as a salt when computing a shadow password.
+ * @param buf buffer for encrypted password with length of MD5_PASSWD_LEN.
+ * Output format is "md5" followed by a 32-hex-digit MD5 checksum.
+ */
+void
+md5_encrypt(const char *password, size_t password_len,
+	    const char *salt, size_t salt_len, char *buf);
