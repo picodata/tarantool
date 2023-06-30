4ad2b98f6ce7ba534656d1d6e7808ecb58dfe929
Dmitry Ivanov <ivadmi5@gmail.com>
2023-06-30 20:18:37 +0300

    feat: Implement LDAP authentication
    This authentication method doesn't store any secrets; instead,
    we delegate the whole auth to a pre-configured LDAP server. In
    the method's implementation, we connect to the LDAP server and
    perform a BIND operation which checks user's credentials.
    
    Usage example:
    
    ```lua
    -- Set the default auth method to LDAP and create a new user.
    -- NOTE that we still have to provide a dummy password; otherwise
    -- box.schema.user.create will setup an empty auth data.
    box.cfg({auth_type = 'ldap'})
    box.schema.user.create('demo', { password = '' })
    
    -- Configure LDAP server connection URL and DN format string.
    os = require('os')
    os.setenv('TT_LDAP_URL', 'ldap://localhost:1389')
    os.setenv('TT_LDAP_DN_FMT', 'cn=$USER,ou=users,dc=example,dc=org')
    
    -- Authenticate using the LDAP authentication method via net.box.
    conn = require('net.box').connect(uri, {
        user = 'demo',
        password = 'password',
        auth_type = 'ldap',
    })
    ```
    
    NO_DOC=internal
    NO_TEST=internal
    NO_CHANGELOG=internal

---
+++ cmake/BuildLDAP.cmake
@@ -0,0 +1,79 @@
+#
+# A macro to build libldap
+macro(ldap_build)
+    message(STATUS "Choosing bundled LDAP")
+
+    # https://git.openldap.org/openldap/openldap
+    set(LDAP_VERSION 2.6.4)
+    set(LDAP_HASH fee2b0dca212b41c87976d0414f30f12)
+    set(LDAP_URL https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-${LDAP_VERSION}.tgz)
+
+    # Reusing approach from BuildLibCURL.cmake
+    get_filename_component(OPENSSL_INSTALL_DIR ${OPENSSL_INCLUDE_DIR} DIRECTORY)
+
+    include(ExternalProject)
+    ExternalProject_Add(bundled-ldap
+        DEPENDS ${LDAP_OPENSSL_DEPS} bundled-sasl
+        URL ${LDAP_URL}
+        URL_MD5 ${LDAP_HASH}
+        PATCH_COMMAND
+            # OpenLDAP builds everything (including MAN pages) unconditionally,
+            # thus we have to patch its sources so as not to install soelim (Groff).
+            sed -i.old "/SUBDIRS/s/clients servers tests doc//" Makefile.in
+        CONFIGURE_COMMAND <SOURCE_DIR>/configure
+            "CC=${CMAKE_C_COMPILER}"
+            "CFLAGS=${DEPENDENCY_CFLAGS}"
+            "CPPFLAGS=${DEPENDENCY_CPPFLAGS} -I${OPENSSL_INCLUDE_DIR} -I${SASL_INCLUDE_DIR}"
+            "LDFLAGS=-L${OPENSSL_INSTALL_DIR}/lib -L${SASL_INSTALL_DIR}/lib"
+            "LIBS=-lssl -lcrypto -ldl -lpthread"
+            --prefix=<INSTALL_DIR>
+
+            --with-cyrus-sasl
+            --with-tls=openssl
+
+            --enable-static
+            --disable-shared
+
+            --enable-local
+            --enable-ipv6
+
+            --disable-debug
+            --disable-slapd
+    )
+
+    unset(OPENSSL_INSTALL_DIR)
+
+    ExternalProject_Get_Property(bundled-ldap install_dir)
+    set(LDAP_INSTALL_DIR ${install_dir})
+    unset(install_dir)
+
+    # Unfortunately, we can't use find_library here,
+    # since the package hasn't been built yet.
+    # We set the same vars as in FindLDAP.cmake.
+    set(LDAP_FOUND TRUE)
+    set(LDAP_INCLUDE_DIR ${LDAP_INSTALL_DIR}/include)
+    set(LDAP_LIBRARIES
+        ${LDAP_INSTALL_DIR}/lib/libldap.a
+        ${LDAP_INSTALL_DIR}/lib/liblber.a
+    )
+
+    ExternalProject_Add_Step(bundled-ldap byproducts
+        BYPRODUCTS ${LDAP_LIBRARIES})
+
+    # On OS X we may also need to link libresolv.dylib.
+    # However, note that newer OS X releases don't have that file;
+    # instead, it's provided via a built-in dynamic linker cache.
+    if(APPLE)
+        find_library(RESOLV_LIBRARIES NAMES resolv PATHS
+            /usr/lib
+            /usr/local/opt/openldap/lib
+            /usr/local/lib
+            NO_CMAKE_SYSTEM_PATH)
+        if(NOT "${RESOLV_LIBRARIES}" STREQUAL "RESOLV_LIBRARIES-NOTFOUND")
+            set(LDAP_LIBRARIES ${LDAP_LIBRARIES} ${RESOLV_LIBRARIES})
+        endif()
+    endif()
+
+    message(STATUS "Found LDAP includes: ${LDAP_INCLUDE_DIR}")
+    message(STATUS "Found LDAP libraries: ${LDAP_LIBRARIES}")
+endmacro()
