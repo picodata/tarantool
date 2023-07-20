d6f84693a6d571aed491a858c5e7ad8523684a6c
Kaimtazian Maksim <m.kaitmazian@picodata.io>
2023-07-20 10:25:55 +0300

    feat: make user name argument optional
    part of picodata/tarantool#21
    
    NO_CHANGELOG=refactoring
    NO_DOC=refactoring

---
+++ test/box-luatest/gl_21_make_user_name_argument_optional_test.lua
@@ -0,0 +1,40 @@
+local server = require('luatest.server')
+local t = require('luatest')
+
+local g = t.group()
+
+g.before_all(function()
+    g.server = server:new({alias = 'master'})
+    g.server:start()
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.test_box_password_without_username_argument = function()
+    t.assert(g.server:exec(function()
+        box.cfg{auth_type='chap-sha1'}
+
+        t.assert_equals(box.schema.user.password("qwerty", box.session.user()),
+                        'qhQg8YLoi55fh09vvnRZKR6PRgE=')
+        t.assert_equals(box.schema.user.password("qwerty"),
+                        box.schema.user.password("qwerty", box.session.user()))
+        --- chap-sha1 doesn't use username arugment for hashing
+        --- so the passwords must be equal
+        t.assert_equals(box.schema.user.password("qwerty"),
+                        box.schema.user.password("qwerty", "???"))
+        box.cfg{auth_type='md5'}
+        t.assert_equals(box.schema.user.password("qwerty", ""),
+                        'md5d8578edf8458ce06fbc5bb76a58c5ca4')
+        t.assert_equals(box.schema.user.password("qwerty"),
+                        box.schema.user.password("qwerty", box.session.user()))
+        t.assert_equals(box.schema.user.password("qwerty", "max"),
+                        "md5a49f3a4f559abc1c8d96547aa3809e90")
+        --- md5 uses username arugment for hashing
+        --- so the passwords must not be equal
+        t.assert_not_equals(box.schema.user.password("qwerty"),
+                            box.schema.user.password("qwerty", "???"))
+        return true
+    end))
+end
