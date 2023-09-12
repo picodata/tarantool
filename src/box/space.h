748381ac0b1e309ede94221dd7abc26aa4a3c955
Dmitry Rodionov <d.rodionov@picodata.io>
2023-09-12 14:45:45 +0300

    feat: export box_access_check_space
    NO_DOC=picodata internal patch
    NO_CHANGELOG=picodata internal patch
    NO_TEST=picodata internal patch
    
    Co-authored-by: Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>

--- src/box/space.h
+++ src/box/space.h
@@ -482,6 +482,10 @@ index_name_by_id(struct space *space, uint32_t id);
 /**
  * Check whether or not the current user can be granted
  * the requested access to the space.
+ * @param space Space to run access check against
+ * @param access Requested access
+ * @retval 0 on success when access is granted
+ * @retval -1 on error (check box_error_last())
  */
 int
 access_check_space(struct space *space, user_access_t access);
