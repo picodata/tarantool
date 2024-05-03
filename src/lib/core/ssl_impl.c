9ed51ceaa6b24b181bcfee8c8d5f5a7e32b990cc
godzie44 <godzie@yandex.ru>
2024-05-03 12:36:07 +0300

    doc: add copyrights for iproto-ssl sources
    NO_DOC=internal
    NO_TEST=internal
    NO_CHANGELOG=internal

--- src/lib/core/ssl_impl.c
+++ src/lib/core/ssl_impl.c
@@ -1,7 +1,12 @@
-/*
- * SPDX-License-Identifier: BSD-2-Clause
+/**
+ * Copyright (C) Picodata LLC - All Rights Reserved
  *
- * Copyright 2010-2021, Tarantool AUTHORS, please see AUTHORS file.
+ * This source code is protected under international copyright law.  All rights
+ * reserved and protected by the copyright holders.
+ * This file is confidential and only available to authorized individuals with
+ * the permission of the copyright holders.  If you encounter this file and do
+ * not have permission, please contact the copyright holders and delete this
+ * file.
  */
 #include "ssl.h"
 
