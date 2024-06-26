9cb8e19fac4a1381e03170116009b44fdb13c115
Denis Smirnov <sd@picodata.io>
2024-06-26 16:15:43 +0700

    refactor: change exported C API for SQL
    BREAKING CHANGE!:
    1. sql_bind_list_decode - removed
    2. sql_execute_prepared_ext - new arguments
    3. sql_prepare_and_execute_ext - exported
    
    There were several reasons to refactor the API.
    1. sql_bind_list_decode (decodes message pack parameters into
       internal C bind structure) is very difficult to use without
       memory leaks (as it allocates results on fiber()->gc).
    2. sql_execute_prepared_ext missed vdbe step limit in parameters
       and used the default value.
    3. Sometimes SQL queries don't fit into prepared statement cache
       and the user still wants to execute them via a slow pass with
       full compilations from the query text. That was the reason to
       export sql_prepare_and_execute_ext symbol.
    
    NO_DOC=internal
    NO_TEST=internal
    NO_CHANGELOG=internal

---
+++ test/sql-luatest/sql_api_test.lua
@@ -0,0 +1,121 @@
+local server = require('luatest.server')
+local t = require('luatest')
+local g = t.group()
+
+g.before_all(function()
+    local ffi_init = [[
+        struct iovec_impl {
+            void *   iov_base;
+            size_t   iov_len;
+        };
+
+        struct obuf_impl
+        {
+                struct slab_cache *slabc;
+                int pos;
+                int n_iov;
+                size_t used;
+                size_t start_capacity;
+                size_t capacity[32];
+                struct iovec_impl iov[32];
+                bool reserved;
+        };
+
+        struct slab_cache * cord_slab_cache(void);
+
+        void obuf_create(
+            struct obuf *buf,
+            struct slab_cache *slabc,
+            size_t start_capacity);
+
+        void obuf_destroy(struct obuf *buf);
+
+        int sql_prepare_and_execute_ext(
+            const char *sql, int len, const char *mp_params,
+            uint64_t vdbe_max_steps, struct obuf *out_buf);
+
+        int sql_execute_prepared_ext(
+            uint32_t stmt_id, const char *mp_params,
+            uint64_t vdbe_max_steps, struct obuf *out_buf);
+
+    ]]
+    g.server = server:new({alias = 'sql_api'})
+    g.server:start()
+    g.server:exec(function(cfg)
+        local ffi = require('ffi')
+        ffi.cdef(cfg)
+    end, {ffi_init})
+end)
+
+g.after_all(function()
+    g.server:stop()
+end)
+
+g.before_each(function()
+    g.server:exec(function()
+        box.execute([[CREATE TABLE t(a text NOT NULL PRIMARY KEY)]])
+    end)
+end)
+
+g.after_each(function()
+    g.server:exec(function()
+        box.execute([[DROP TABLE t]])
+    end)
+end)
+
+g.test_stmt_preparetion_and_execution = function()
+    g.server:exec(function()
+        local res, buf
+
+        local ffi = require('ffi')
+        local slab_cache = ffi.C.cord_slab_cache()
+        t.assert_not_equals(slab_cache, ffi.NULL)
+        local mem_chunk = ffi.new("struct obuf_impl[1]")
+        local obuf = ffi.cast('struct obuf *', mem_chunk)
+        ffi.C.obuf_create(obuf, slab_cache, 1024)
+
+        -- Prepare and execute the statement with C API.
+        -- '\x91\xd9\x01C' is a msgpack representation of ['A'].
+        -- \x91 is an array of 1 element.
+        -- \xd9 is a string with 1 byte length.
+        -- \x01 is the length of the string 'A'.
+        buf = ffi.cast('char *', '\x91\xd9\x01A')
+        res = ffi.C.sql_prepare_and_execute_ext(
+            'INSERT INTO t VALUES (?)', 24, buf, 1024, obuf)
+        t.assert_equals(res, 0)
+
+        -- Check the result.
+        res = box.execute([[SELECT * FROM t WHERE a = 'A']])
+        t.assert_equals(res.rows[1][1], 'A')
+
+        ffi.C.obuf_destroy(obuf)
+    end)
+end
+
+g.test_prepared_stmt_execution = function()
+    g.server:exec(function()
+        local res, buf
+
+        local ffi = require('ffi')
+        local slab_cache = ffi.C.cord_slab_cache()
+        t.assert_not_equals(slab_cache, ffi.NULL)
+        local mem_chunk = ffi.new("struct obuf_impl[1]")
+        local obuf = ffi.cast('struct obuf *', mem_chunk)
+        ffi.C.obuf_create(obuf, slab_cache, 1024)
+
+        -- Prepare the statement.
+        local s = box.prepare('INSERT INTO t VALUES (?)')
+
+        -- Prepare and execute the statement with C API.
+        buf = ffi.cast('char *', '\x91\xd9\x01B')
+        res = ffi.C.sql_execute_prepared_ext(s.stmt_id, buf, 1024, obuf)
+        t.assert_equals(res, 0)
+
+        -- Check the result.
+        res = box.execute([[SELECT * FROM t WHERE a = 'B']])
+        t.assert_equals(res.rows[1][1], 'B')
+
+        ffi.C.obuf_destroy(obuf)
+    end)
+end
+
