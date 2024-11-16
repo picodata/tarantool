727bff4abb1aa2f6aaf6b4b9af957f7ec259099b
Dmitry Ivanov <ivadmi5@gmail.com>
2024-11-16 03:42:57 +0300

    fix: don't close stderr & stdout in log_destroy
    Previously, log_destroy would close log->fd even if it's one of the
    standard streams. This behavior almost never makes sense, unless
    one's trying to write a unix daemon which is obviously not the case.
    
    Closing stderr has the following side effects:
    - it breaks a reinit of the default logger;
    - it inhibits ASan's final leak report;
    - it causes a EPOLLHUP during a restart via `--entrypoint-fd` (Picodata).
    
    NO_DOC=internal
    NO_TEST=internal
    NO_CHANGELOG=internal

--- src/lib/core/say.c
+++ src/lib/core/say.c
@@ -1409,11 +1409,17 @@ log_destroy(struct log *log)
 	while(log->rotating_threads > 0)
 		tt_pthread_cond_wait(&log->rotate_cond, &log->rotate_mutex);
 	tt_pthread_mutex_unlock(&log->rotate_mutex);
+	pm_atomic_store(&log->type, SAY_LOGGER_BOOT);
+
 	/*
-	 * Do not close stderr because it's used for reporting crashes
-	 * and memory leaks.
+	 * We shouldn't close standard IO streams.
+	 * Doing so will:
+	 * - break a reinit of the default logger
+	 * - inhibit ASan's final leak report
+	 * - cause a EPOLLHUP during a restart via `--entrypoint-fd`.
 	 */
-	if (log->type != SAY_LOGGER_STDERR)
+	bool is_std = log->fd == STDOUT_FILENO || log->fd == STDERR_FILENO;
+	if (log->fd != -1 && !is_std)
 		close(log->fd);
 	free(log->syslog_ident);
 	free(log->path);
