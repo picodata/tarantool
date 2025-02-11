5c86f95283b6b2de1bdeb15ea11133cb08896600
Dmitry Ivanov <ivadmi5@gmail.com>
2025-02-11 23:03:20 +0300

    fix: cumulative fixes to window functions
    Based on the following sqlite commits:
    - e4984a2
    - 287fa17
    - 7a606e1
    - e33f6e7
    - 683b0ff
    - f607bec
    - 7262ca9
    
    NO_DOC=internal
    NO_CHANGELOG=internal

--- test/sql-tap/window6.test.lua
+++ test/sql-tap/window6.test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env tarantool
 local test = require("sqltester")
-test:plan(8)
+test:plan(22)
 
 test:execsql([[
     DROP TABLE IF EXISTS over;
@@ -51,6 +51,111 @@ SELECT sum(window) OVER window  window FROM window  window  window  window AS (O
     }
 )
 
+test:do_execsql_test(
+    "5.5",
+    [[
+SELECT count(*) OVER win FROM over
+WINDOW win AS (ORDER BY x ROWS BETWEEN +2 FOLLOWING AND +3 FOLLOWING)
+    ]],
+    {
+        1,
+        0,
+        0,
+    }
+)
+
+test:do_execsql_test(
+    "8.0",
+    [[
+CREATE TABLE IF NOT EXISTS "sample" (
+      "id" INTEGER NOT NULL PRIMARY KEY,
+      "counter" INTEGER NOT NULL,
+      "value" DOUBLE NOT NULL
+);
+INSERT INTO "sample" ("id", "counter", "value")
+VALUES (1, 1, 10.), (2, 1, 20.), (3, 2, 1.), (4, 2, 3.), (5, 3, 100.);
+    ]],
+    {}
+)
+
+test:do_execsql_test(
+    "8.1",
+    [[
+SELECT "counter", "value", row_number() OVER w AS row_number
+FROM "sample"
+WINDOW w AS (PARTITION BY "counter" ORDER BY "value" DESC)
+ORDER BY "counter", row_number() OVER w
+    ]],
+    {
+        1, 20.0, 1,
+        1, 10.0, 2,
+        2, 3.0, 1,
+        2, 1.0, 2,
+        3, 100.0, 1,
+    }
+)
+
+test:do_catchsql_test(
+    "9.0",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT x, group_concat(cast (x as text)) OVER (ORDER BY x ROWS 2 PRECEDING)
+FROM c;
+    ]],
+    { 0, {
+        1, "1",
+        2, "1,2",
+        3, "1,2,3",
+        4, "2,3,4",
+        5, "3,4,5",
+    }}
+)
+
+test:do_catchsql_test(
+    "9.0.1",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT x, group_concat(cast (x as text), NULL) OVER (ORDER BY x ROWS 2 PRECEDING)
+FROM c;
+    ]],
+    { 0, {
+        1, "1",
+        2, "12",
+        3, "123",
+        4, "234",
+        5, "345",
+    }}
+)
+
+test:do_catchsql_test(
+    "9.1",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT x, group_concat(x) OVER (ORDER BY x RANGE 2 PRECEDING)
+FROM c;
+    ]],
+    { 1, "RANGE must use only UNBOUNDED or CURRENT ROW" }
+)
+
+test:do_catchsql_test(
+    "9.2",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT x, group_concat(x) OVER (ORDER BY x RANGE BETWEEN UNBOUNDED PRECEDING AND 2 FOLLOWING)
+FROM c;
+    ]],
+    { 1, "RANGE must use only UNBOUNDED or CURRENT ROW" }
+)
+
+test:do_catchsql_test(
+    "9.3",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT count(DISTINCT x) OVER (ORDER BY x) FROM c;
+    ]],
+    { 1, "DISTINCT is not supported for window functions" }
+)
+
 test:do_catchsql_test(
     "9.4",
     [[
@@ -66,7 +171,7 @@ test:do_catchsql_test(
 WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
 SELECT count() OVER (ORDER BY x RANGE BETWEEN UNBOUNDED FOLLOWING AND UNBOUNDED FOLLOWING) FROM c;
     ]],
-    { 1, "Syntax error at line 2 near 'UNBOUNDED'" }
+    { 1, "Syntax error at line 2 near 'FOLLOWING'" }
 )
 
 test:do_catchsql_test(
@@ -76,6 +181,46 @@ WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
 SELECT count() OVER (ORDER BY x RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED PRECEDING) FROM c;
     ]],
     { 1, "Syntax error at line 2 near 'PRECEDING'" }
+)
+
+for i, frame_bound in ipairs({
+    "BETWEEN CURRENT ROW AND 4 PRECEDING",
+    "4 FOLLOWING",
+    "BETWEEN 4 FOLLOWING AND CURRENT ROW",
+    "BETWEEN 4 FOLLOWING AND 2 PRECEDING",
+}) do
+    test:do_catchsql_test(
+        "9.6." .. i,
+        string.format([[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT count() OVER (
+    ORDER BY x ROWS %s
+) FROM c;
+        ]], frame_bound),
+        { 1, "unsupported window-frame type" }
+    )
+end
+
+test:do_catchsql_test(
+    "9.8.1",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT count() OVER (
+  ORDER BY x ROWS BETWEEN a PRECEDING AND 2 FOLLOWING
+) FROM c;
+    ]],
+    { 1, "frame starting offset must be a non-negative integer" }
+)
+
+test:do_catchsql_test(
+    "9.8.2",
+    [[
+WITH RECURSIVE c(x) AS (VALUES(1) UNION ALL SELECT x+1 FROM c WHERE x<5)
+SELECT count() OVER (
+  ORDER BY x ROWS BETWEEN 2 PRECEDING AND a FOLLOWING
+) FROM c;
+    ]],
+    { 1, "frame ending offset must be a non-negative integer" }
 )
 
 test:finish_test()
