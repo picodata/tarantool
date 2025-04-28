4f7ad62b0bacdb814dbd882e5cc16ec1e249cbd8
Nikita Strygin <n.strygin@picodata.io>
2025-04-28 17:55:28 +0300

    fix(wal_ext): make WAL extensions into a non-dynamic config initialized early
    We have to do this before any of the spaces get initialized.
    
    Otherwise the spaces were initialized before the appropriate wal extensions
    were configured, leaving all of the spaces not created during the session
    unaffected by the extensions.
    
    To implement this, change the extensions configuration from using a table
    to using `wal_extensions_config` structure. Add `cfg_get_wal_ext` to
    `cfg.h` that parses the extensions configuration table, like
    `cfg_get_uri_set`. Replace exported `cfg_set_wal_ext` lua function with
    a stub.
    
    NO_DOC=picodata
    NO_CHANGELOG=picodata

--- src/box/wal_ext_impl.c
+++ src/box/wal_ext_impl.c
@@ -14,38 +14,9 @@ struct space_wal_ext {
 	void (*process)(struct txn_stmt *stmt, struct request *request);
 };
 
-/** List of wal extensions. */
-enum wal_extension {
-	NEW_OLD_TUPLE,
-	wal_extension_MAX
+static struct wal_extensions_config global_extensions = {
+	.new_old = false,
 };
-
-static const char *const wal_extension_strs[] = {
-	[NEW_OLD_TUPLE] = "new_old",
-};
-
-static bool global_extensions[] = {
-	[NEW_OLD_TUPLE] = false,
-	[wal_extension_MAX] = false,
-};
-
-static inline enum wal_extension
-extension_by_name(const char *name)
-{
-	return STR2ENUM(wal_extension, name);
-}
-
-int
-set_enable_extension(const char *ext_name, bool enable)
-{
-	enum wal_extension ext = extension_by_name(ext_name);
-	if (ext == wal_extension_MAX) {
-		return -1;
-	}
-
-	global_extensions[ext] = enable;
-	return 0;
-}
 
 void
 append_old_and_new_tuple(struct txn_stmt *stmt, struct request *request)
@@ -90,6 +61,71 @@ wal_ext_free(void)
 {
 }
 
+int
+luaT_wal_ext_config_create(struct lua_State *L, int idx,
+			   struct wal_extensions_config *ext_config)
+{
+	memset(ext_config, 0, sizeof(struct wal_extensions_config));
+
+	if (idx < 0)
+		idx = lua_gettop(L) + idx + 1;
+	assert(idx > 0);
+
+	if (lua_isnil(L, idx))
+		return 0;
+
+	bool is_table = lua_istable(L, idx);
+	if (!is_table) {
+		diag_set(IllegalParams,
+			 "`wal_ext` value must be a table or nil");
+		return -1;
+	}
+
+	/* parse the wal_ext table as a set of
+	 *     {[extension] = [enabled]} pairs
+	 */
+	lua_pushnil(L);
+	/* "wal_ext" table now at -2 */
+	while (lua_next(L, idx) != 0) {
+		const char *ext_name = lua_tostring(L, -2); /* key */
+
+		if (!lua_isboolean(L, -1)) {
+			diag_set(IllegalParams,
+				 "value for extension `%s` must be a boolean",
+				 ext_name);
+			lua_pop(L, 2); /* pop `enable` and `key` */
+			return -1;
+		}
+		bool enable = lua_toboolean(L, -1); /* value */
+
+		if (strcmp(ext_name, "new_old") == 0) {
+			ext_config->new_old = enable;
+		} else {
+			diag_set(IllegalParams,
+				 "extension `%s` does not exist",
+				 ext_name);
+			lua_pop(L, 2); /* pop `enable` and `key` */
+			return -1;
+		}
+
+		/* pop `enable` */
+		lua_pop(L, 1);
+	}
+
+	/* lua_next leaves a clean stack when iteration finishes */
+
+	return 0;
+}
+
+void
+wal_ext_set_cfg(struct wal_extensions_config *ext_config)
+{
+	if (ext_config->new_old)
+		say_info("Enabling new_old WAL extension");
+
+	global_extensions = *ext_config;
+}
+
 void
 space_wal_ext_process_request(struct space_wal_ext *ext, struct txn_stmt *stmt,
 			      struct request *request)
@@ -102,7 +138,7 @@ struct space_wal_ext *
 space_wal_ext_by_name(const char *space_name)
 {
 	(void)space_name;
-	if (global_extensions[NEW_OLD_TUPLE]) {
+	if (global_extensions.new_old) {
 		return &new_old_ext;
 	}
 	return NULL;
--- test/box-luatest/wal_ext_test.lua
+++ test/box-luatest/wal_ext_test.lua
@@ -4,8 +4,31 @@ local replica_set = require('luatest.replica_set')
 
 local g = t.group()
 
-g.test_new_old_extension_wal_format = function()
-    g.server = server:new({ alias = 'master' })
+g.test_wal_ext_not_dynamic = function()
+        -- first configure with wal_ext = nil
+    g.server = server:new({ alias = 'master', box_cfg = { wal_ext = nil } })
+    g.server:start()
+
+    g.server:exec(function()
+        -- then try to change it
+        local res, err = pcall(function()
+            box.cfg { wal_ext = { new_old = true } }
+        end)
+
+        -- verify that tarantool refuses to change it
+        t.assert(res == false)
+        t.assert(err:match("Can't set option 'wal_ext' dynamically"))
+    end)
+
+    g.server:stop()
+end
+
+g.test_new_old_extension_enabled = function()
+    -- enable new_old extension
+    g.server = server:new({
+        alias = 'master',
+        box_cfg = { wal_ext = { new_old = true } },
+    })
     g.server:start()
 
     g.server:exec(function()
@@ -20,10 +43,6 @@ g.test_new_old_extension_wal_format = function()
             return val
         end
 
-        box.cfg({})
-
-        -- enable new_old extension
-        box.cfg { wal_ext = { new_old = true } }
         box.schema.space.create('test'):create_index('pk')
 
         -- generate a new xlog
@@ -61,13 +80,36 @@ g.test_new_old_extension_wal_format = function()
                 and table.equals(data[4].BODY.old_tuple:totable(), { 1, "1" })
         )
 
-        -- disable new_old wal extension
-        box.cfg { wal_ext = { new_old = false } }
+        box.space.test:drop()
+    end)
 
+    g.server:stop()
+end
+
+g.test_new_old_extension_disabled = function()
+    -- disable new_old extension
+    g.server = server:new({
+        alias = 'master',
+        box_cfg = { wal_ext = { new_old = false } },
+    })
+    g.server:start()
+
+    g.server:exec(function()
+        local fio = require('fio')
+        local xlog = require('xlog').pairs
+
+        local function read_xlog(file)
+            local val = {}
+            for _, v in xlog(file) do
+                table.insert(val, v)
+            end
+            return val
+        end
+
+        -- generate a new xlog
+        box.snapshot()
         local lsn = box.info.lsn
-
-        -- drop space cache
-        box.space.test:alter({})
+        box.schema.space.create('test'):create_index('pk')
 
         box.space.test:insert({ 3, "3" })
         box.space.test:update(3, { { '=', 2, '4' } })
@@ -83,17 +125,17 @@ g.test_new_old_extension_wal_format = function()
                 string.format('%020d.xlog', lsn)
         )
         local data = read_xlog(log_path)
-        t.assert(data[2].HEADER.type == 'INSERT'
-                and data[2].BODY.new_tuple == nil
-                and data[2].BODY.old_tuple == nil
-        )
-        t.assert(data[3].HEADER.type == 'UPDATE'
+        t.assert(data[3].HEADER.type == 'INSERT'
                 and data[3].BODY.new_tuple == nil
                 and data[3].BODY.old_tuple == nil
         )
-        t.assert(data[4].HEADER.type == 'DELETE'
+        t.assert(data[4].HEADER.type == 'UPDATE'
                 and data[4].BODY.new_tuple == nil
                 and data[4].BODY.old_tuple == nil
+        )
+        t.assert(data[5].HEADER.type == 'DELETE'
+                and data[5].BODY.new_tuple == nil
+                and data[5].BODY.old_tuple == nil
         )
 
         box.space.test:drop()
@@ -104,7 +146,7 @@ end
 
 g.test_new_old_extension_replicated = function()
     g.rs = replica_set:new()
-    g.box_cfg = {
+    g.master_box_cfg = {
         replication_timeout = 0.1,
         replication_connect_timeout = 10,
         replication_sync_lag = 0.01,
@@ -112,18 +154,34 @@ g.test_new_old_extension_replicated = function()
         replication = {
             server.build_listen_uri('master', g.rs.id),
             server.build_listen_uri('replica', g.rs.id),
-        }
+        },
+        -- enable new_old extension only on master
+        wal_ext = { new_old = true },
+    }
+    g.replica_box_cfg = {
+        replication_timeout = 0.1,
+        replication_connect_timeout = 10,
+        replication_sync_lag = 0.01,
+        replication_connect_quorum = 3,
+        replication = {
+            server.build_listen_uri('master', g.rs.id),
+            server.build_listen_uri('replica', g.rs.id),
+        },
     }
 
-    g.rs:build_and_add_server({ alias = 'master', box_cfg = g.box_cfg })
-    g.rs:build_and_add_server({ alias = 'replica', box_cfg = g.box_cfg })
+    g.rs:build_and_add_server({
+        alias = 'master',
+        box_cfg = g.master_box_cfg
+    })
+    g.rs:build_and_add_server({
+        alias = 'replica',
+        box_cfg = g.replica_box_cfg
+    })
     g.rs:start()
 
     local lsn = g.rs:get_server('master'):exec(function()
         local lsn = box.info.lsn
 
-        -- enable new_old extension
-        box.cfg { wal_ext = { new_old = true } }
         box.schema.space.create('test'):create_index('pk')
 
         box.space.test:insert({ 1, "1" })
