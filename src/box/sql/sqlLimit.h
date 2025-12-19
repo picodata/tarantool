57c03f592233c6de57e9cc12b8784affc2b70e9d
Denis Smirnov <sd@picodata.io>
2025-12-19 14:08:04 +0700

    sql: add vdbe execution callback
    Added a VDBE execution callback that runs every OPCODE_YIELD_COUNT
    opcodes. It receives the monotonic system time so user code can
    react based on elapsed execution time.
    
    NO_DOC=internal
    NO_TEST=internal
    NO_CHANGELOG=internal

--- src/box/sql/sqlLimit.h
+++ src/box/sql/sqlLimit.h
@@ -127,6 +127,21 @@ enum {
  */
 extern uint64_t default_vdbe_max_steps;
 
+/* The amount of opcodes to execute before running vdbe_yield_cb. */
+extern const uint16_t OPCODE_YIELD_COUNT;
+
+/* Arguments for vdbe_yield_cb callback. */
+struct vdbe_yield_args {
+	/* Time since the last fiber switch, controlled by vdbe callback. */
+	int64_t *start;
+	/* Current time. */
+	int64_t current;
+};
+
+/* Callback to execute custom logic while running vdbe. */
+extern int
+(*vdbe_yield_cb)(struct vdbe_yield_args *args);
+
 /*
  * The maximum number of arguments to an SQL function.
  */
