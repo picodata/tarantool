638b3a68536c32e410dedd42e62113ac32a30f33
Konstantin Osipov <kostja.osipov@gmail.com>
2026-01-16 04:35:45 -0500

    vinyl: stabilize vinyl/*.test.lua
    The tests often depend on the precise behaviour of the compaction
    scheduler, which depends on dumps, compression impact and is explicitly
    randomized with a random seed for each slice.
    
    Even if the random seed is set in the test the slice seed depends on
    concurrency of background fibers and vinyl engine. Make sure the
    use of rand() in fiber.c doesn't impact vinyl, since these two
    invocations are based off different seeds, which are in turn created
    in a fixed order (initialization order).
    
    See for example errin_stat.test.lua, stat.test.lua, ddl.test.lua.
    Tested by running the above tests 500 times each.
    
    NO_DOC=Picodata
    NO_CHANGELOG=Picodata

--- test/vinyl/stat.lua
+++ test/vinyl/stat.lua
@@ -1,8 +1,34 @@
 #!/usr/bin/env tarantool
+--
+-- The compaction scheduler sometimes randomly pessimises
+-- the compaction rules based on slice->seed, which is generated
+-- using rand() for each slice. Set the random seed to ensure the
+-- randomness kicks in exactly the same slices from run to run.
+-- Run *before* box.cfg{} to ensure slice->rand is correct
+-- even after server restart.
+--
+local ffi = require('ffi')
+ffi.cdef('void srand(unsigned int seed);')
+ffi.C.srand(1)
 
 box.cfg{
     vinyl_cache = 15 * 1024, -- 15K to test cache eviction
     vinyl_defer_deletes = true,
+    --
+    -- While the default checkpoint interval is 1 hour,
+    -- it is still possible that a checkpoint happens quickly
+    -- after the server starts. This will increase the number
+    -- of dumps, may lead to an extra compaction and impact
+    -- metrics.
+    checkpoint_interval=0,
 }
+
+--
+-- The test generates random data. The data is then
+-- compressed and compression rate differs from seed to seed.
+-- This may impact the compaction scheduler, which is based
+-- on compressed size, not binary size.
+--
+math.randomseed(1)
 
 require('console').listen(os.getenv('ADMIN'))
