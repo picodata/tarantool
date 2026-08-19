picodata/tarantool

commit 5f343cf45722a6fc8a668d80abcb15ddb78e0664

fix: timezone not nul-terminated
When using the `datetime` cast, the time zone passed to the
`timezone_raw_lookup` function was not guaranteed to be NUL-terminated,
which led to an incorrect lookup in the binary search, since it used
`strcasecmp`.

src/lib/tzcode/timezone.c
@@ -12,6 +12,11 @@
 #include "trivia/util.h"
 
 /**
+ * Maximum length of a time zone name.
+ */
+#define TZ_NAME_MAX 128
+
+/**
  * Array of zone descriptors, placed in their natural id order, used for
  * translation from zone id to unique zone name and their attributes.
  */
@@ -122,7 +127,7 @@ timezone_free(void)
 /**
  * We want to accept only names in a form:
  * - Z, AT, MSK, i.e. [A-Z]{1,6}
- * - Etc/GMT, Europe/Moscow, America/St_Kitts, i.e. [A-Za-z][A-Za-z/_-]*
+ * - Etc/GMT, Europe/Moscow, America/St_Kitts, GMT+03:00, i.e. [A-Za-z][A-Za-z/_-\+:]*
  * NB! Eventually should be reimplemented with proper regexp, but now
  * it accepts slightly wider class of input.
  */
@@ -135,7 +140,7 @@ char_span_alpha(const char *src, size_t len)
 		return 0;
 	for (n = 0; n < len; n++) {
 		char ch = src[n];
-		if (!isalpha(ch) && ch != '/' && ch != '_' && ch != '-')
+		if (!isalpha(ch) && ch != '/' && ch != '_' && ch != '-' && ch != '+' && ch == ':')
 			break;
 
 	}
@@ -150,7 +155,14 @@ timezone_raw_lookup(const char *str, size_t len,
 	if (len == 0)
 		return 0;
 
-	struct date_time_zone wrap = {.name = str};
+	/* we need NUL-terminated string for strcasecmp in bsearch */
+	if (len > TZ_NAME_MAX)
+		return 0;
+	char key[TZ_NAME_MAX + 1];
+	memcpy(key, str, len);
+	key[len] = '\0';
+
+	struct date_time_zone wrap = {.name = key};
 	struct date_time_zone *found = (struct date_time_zone *)
 		bsearch(&wrap, zones_sorted, lengthof(zones_sorted),
 			sizeof(zones_sorted[0]), compare_zones);

test/unit/datetime.c
@@ -76,12 +76,18 @@ struct {
 	S("2012-12-24 14:30 UTC-1"),
 	S("2012-12-24 14:30 UTC-01"),
 	S("2012-12-24 14:30 UTC-01:00"),
+	S("2012-12-24 14:30 UTC−09:00"),
+	S("2012-12-24 14:30 UTC−09:30"), /* Marquesas */
 	S("2012-12-24 14:30 UTC-0100"),
 	S("2012-12-24 15:30 GMT"),
 	S("2012-12-24 16:30 GMT+1"),
 	S("2012-12-24 16:30 GMT+01"),
 	S("2012-12-24 16:30 GMT+0100"),
 	S("2012-12-24 16:30 GMT+01:00"),
+	S("2012-12-24 16:30 GMT+03:00"),
+	S("2012-12-24 16:30 GMT+03:30"), /* Iran */
+	S("2012-12-24 16:30 GMT+05:00"),
+	S("2012-12-24 16:30 GMT+05:45"), /* Nepal */
 	S("2012-12-24 14:30 GMT-1"),
 	S("2012-12-24 14:30 GMT-01"),
 	S("2012-12-24 14:30 GMT-01:00"),
@@ -105,7 +111,7 @@ datetime_test(void)
 	size_t index;
 	struct datetime date_expected;
 
-	plan(497);
+	plan(539);
 	datetime_parse_full(&date_expected, sample, sizeof(sample) - 1);
 
 	for (index = 0; index < lengthof(tests); index++) {
@@ -583,10 +589,45 @@ interval_from_map_test(void)
 	check_plan();
 }
 
+static void
+datetime_parse_unterminated_test(void)
+{
+	plan(4);
+	header();
+
+	const char *samples[] = {
+		"2012-12-24T15:30:00Z",
+		"2012-12-24 15:30 MSK",
+	};
+
+	for (size_t i = 0; i < lengthof(samples); i++) {
+		const char *s = samples[i];
+		size_t len = strlen(s);
+
+		char buf[64];
+		fail_if(len >= sizeof(buf));
+		memset(buf, 'x', sizeof(buf));
+		memcpy(buf, s, len);
+
+		struct datetime got;
+		ssize_t rc = datetime_parse_full(&got, buf, len);
+		is(rc > 0, true, "parses trailing zone without reading past "
+		   "len for '%s'", s);
+
+		struct datetime ref;
+		datetime_parse_full(&ref, s, len);
+		is(got.epoch, ref.epoch,
+		   "same epoch as NUL-terminated input for '%s'", s);
+	}
+
+	footer();
+	check_plan();
+}
+
 int
 main(void)
 {
-	plan(7);
+	plan(8);
 	datetime_test();
 	tostring_datetime_test();
 	parse_date_test();
@@ -594,6 +635,7 @@ main(void)
 	mp_datetime_test();
 	mp_print_test();
 	interval_from_map_test();
+	datetime_parse_unterminated_test();
 
 	return check_plan();
 }

commit 20cac7b1463796a973eca12e66c4865b6c3af35a

fix: parsing of colon-separated time zone offsets
Commit 5f343cf45722 ("fix: timezone not nul-terminated") introduced a bug
in the `char_span_alpha` function: the parsing of time zone would stopped
at the first `':'` symbol.

src/lib/tzcode/timezone.c
@@ -140,7 +140,7 @@ char_span_alpha(const char *src, size_t len)
 		return 0;
 	for (n = 0; n < len; n++) {
 		char ch = src[n];
-		if (!isalpha(ch) && ch != '/' && ch != '_' && ch != '-' && ch != '+' && ch == ':')
+		if (!isalpha(ch) && ch != '/' && ch != '_' && ch != '-' && ch != '+' && ch != ':')
 			break;
 
 	}

test/unit/datetime.c
@@ -76,8 +76,8 @@ struct {
 	S("2012-12-24 14:30 UTC-1"),
 	S("2012-12-24 14:30 UTC-01"),
 	S("2012-12-24 14:30 UTC-01:00"),
-	S("2012-12-24 14:30 UTC−09:00"),
-	S("2012-12-24 14:30 UTC−09:30"), /* Marquesas */
+	S("2012-12-24 14:30 UTC-09:00"),
+	S("2012-12-24 14:30 UTC-09:30"), /* Marquesas */
 	S("2012-12-24 14:30 UTC-0100"),
 	S("2012-12-24 15:30 GMT"),
 	S("2012-12-24 16:30 GMT+1"),
@@ -630,10 +630,51 @@ datetime_parse_unterminated_test(void)
 	check_plan();
 }
 
+static void
+datetime_parse_zone_suffix_test(void)
+{
+	struct {
+		const char *str;
+		const char *prefix;
+	} samples[] = {
+		{"2012-12-24 15:30Z 12:00", "2012-12-24 15:30Z"},
+		{"2012-12-24 15:30 MSK 12:00", "2012-12-24 15:30 MSK"},
+		{"2012-12-24 15:30 MSK, note", "2012-12-24 15:30 MSK"},
+		{"2012-12-24 15:30 UTC 12:00", "2012-12-24 15:30 UTC"},
+		{"2012-12-24 15:30 Europe/Moscow 12:00",
+		 "2012-12-24 15:30 Europe/Moscow"},
+		{"2012-12-24 15:30 America/New_York 12:00",
+		 "2012-12-24 15:30 America/New_York"},
+	};
+
+	plan(4 * lengthof(samples));
+	header();
+
+	for (size_t i = 0; i < lengthof(samples); i++) {
+		const char *s = samples[i].str;
+		const char *prefix = samples[i].prefix;
+		size_t prefix_len = strlen(prefix);
+
+		struct datetime got = {.epoch = 0};
+		ssize_t rc = datetime_parse_full(&got, s, strlen(s));
+		is(rc, (ssize_t)prefix_len, "stops at the end of the zone "
+		   "of '%s'", s);
+
+		struct datetime ref = {.epoch = 0};
+		rc = datetime_parse_full(&ref, prefix, prefix_len);
+		is(rc, (ssize_t)prefix_len, "consumes '%s'", prefix);
+		is(got.epoch, ref.epoch, "same epoch as '%s'", prefix);
+		is(got.tzoffset, ref.tzoffset, "same tzoffset as '%s'", prefix);
+	}
+
+	footer();
+	check_plan();
+}
+
 int
 main(void)
 {
-	plan(8);
+	plan(9);
 	datetime_test();
 	tostring_datetime_test();
 	parse_date_test();
@@ -642,6 +683,7 @@ main(void)
 	mp_print_test();
 	interval_from_map_test();
 	datetime_parse_unterminated_test();
+	datetime_parse_zone_suffix_test();
 
 	return check_plan();
 }